af3e963c73a43476478a46c83dfe62ca4c7a0584
NandoSchlemper <bkschlemper@gmail.com>
2025-03-18 21:45:34 -0300

    [feature]: j

---
+++ app/src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: false,
+})
+export class Tab1Page {
+
+  constructor() { }
+
+}
