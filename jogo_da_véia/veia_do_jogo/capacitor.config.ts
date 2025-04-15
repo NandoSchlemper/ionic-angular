4aabc22a365c2550c7fbb423a7b3810f4e525e7c
NandoSchlemper <bkschlemper@gmail.com>
2025-04-15 19:53:54 -0300

    [feature]: First Commit

---
+++ jogo_da_véia/veia_do_jogo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'veia_do_jogo',
+  webDir: 'www'
+};
+
+export default config;
