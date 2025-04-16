7d189b477b2ce277f3d46c357635a188fe8401bb
NandoSchlemper <bkschlemper@gmail.com>
2025-04-15 21:06:17 -0300

    [feature] joguinho da velha falsificado

--- jogo_da_véia/veia_do_jogo/src/app/tab1/tab1.page.spec.ts
+++ jogo_da_véia/veia_do_jogo/src/app/tab1/tab1.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { type ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
--- jogo_da_véia/veia_do_jogo/src/app/tab1/tab1.page.ts
+++ jogo_da_véia/veia_do_jogo/src/app/tab1/tab1.page.ts
@@ -1,13 +1,86 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: false,
 })
+
+
 export class Tab1Page {
+  isActionSheetOpen = false;
+  escolhaUsuario = "";
+  escolheComputador = "";
+  resultadoMensagem = "";
+  public actionSheetButtons = [
+    {
+      text: 'Pedra',
+      handler: () => {
+        this.escolhaUsuario = "pedra"
+        this.jogarPedraPapelTesoura()
+      }
+    },
+    {
+      text: 'Papel',
+      handler: () => {
+        this.escolhaUsuario = "papel"
+        this.jogarPedraPapelTesoura()
+      }
+    },
+    {
+      text: 'Tesoura',
+      handler: () => {
+        this.escolhaUsuario = "tesoura"
+        this.jogarPedraPapelTesoura()
+      }
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
 
-  constructor() {}
+  async jogarPedraPapelTesoura() {
+    const allComputerOptions = ["pedra", "papel", "tesoura"];
+    const indexOfChoise = Math.floor(Math.random() * 3);
+    this.escolheComputador = allComputerOptions[indexOfChoise];
 
+    if (this.escolheComputador === "pedra") {
+      if (this.escolhaUsuario === "pedra") {
+        this.resultadoMensagem = "Empataram, escolheram: pedra";
+      } else if (this.escolhaUsuario === "papel") {
+        this.resultadoMensagem = "Usuário venceu, papel embrulha pedra";
+      } else if (this.escolhaUsuario === "tesoura") {
+        this.resultadoMensagem = "Computador venceu, pedra quebra tesoura";
+      }
+    } 
+    else if (this.escolheComputador === "papel") {
+      if (this.escolhaUsuario === "pedra") {
+        this.resultadoMensagem = "Computador venceu, papel embrulha pedra";
+      } else if (this.escolhaUsuario === "papel") {
+        this.resultadoMensagem = "Empataram, escolheram: papel";
+      } else if (this.escolhaUsuario === "tesoura") {
+        this.resultadoMensagem = "Usuário venceu, tesoura corta papel";
+      }
+    } 
+    else if (this.escolheComputador === "tesoura") {
+      if (this.escolhaUsuario === "pedra") {
+        this.resultadoMensagem = "Usuário venceu, pedra quebra tesoura";
+      } else if (this.escolhaUsuario === "papel") {
+        this.resultadoMensagem = "Computador venceu, tesoura corta papel";
+      } else if (this.escolhaUsuario === "tesoura") {
+        this.resultadoMensagem = "Empataram, escolheram: tesoura";
+      }
+    }
+  }
+  
 }
+
